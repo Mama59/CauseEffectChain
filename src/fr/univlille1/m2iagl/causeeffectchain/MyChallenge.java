b0fe16640bfb514af5496898059842661264400a
Marine Bal <marine.bal.1993@gmail.com>
2016-11-29 15:43:55 +0100

    add My challenge

---
+++ src/fr/univlille1/m2iagl/causeeffectchain/MyChallenge.java
@@ -0,0 +1,30 @@
+package fr.univlille1.m2iagl.causeeffectchain;
+
+public class MyChallenge<I> implements Challenge<I> {
+
+	public Class<? extends I> getInputFormat() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public I getInput1() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public I getInput2() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public boolean oracle(I input) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public CauseEffectChain computeTrace(I x, I y) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
