7631dd5d941f544a85d6b38f2f23a419fb8bf971
rbadr <rahal.badr@gmail.com>
2016-12-24 14:45:08 +0100

    update Main test

---
+++ src/test/java/fr/univlille1/m2iagl/opl/causechaineffect/main/MainTest.java
@@ -0,0 +1,16 @@
+package fr.univlille1.m2iagl.opl.causechaineffect.main;
+
+import org.junit.Test;
+
+public class MainTest {
+
+    /**
+     * Test of main method, of class Main.
+     */
+    @Test
+    public void testMain() {
+        String[] args = null;
+        Main.main(args);
+    }
+    
+}
